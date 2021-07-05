05a5ed7780186c1c9e50de8b75c4773104720fc2
Eduardo Alvarado <eduardoalvara2@gmail.com>
2021-07-05 18:53:52 -0400

    update

--- src/Http/Controllers/API/SheetsController.php
+++ src/Http/Controllers/API/SheetsController.php
@@ -5,53 +5,49 @@ namespace paupololi\sheetsformbuilder\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client as GuzzleClient;
-
+use Auth;
 use Validator;
 
 
 class SheetsController extends Controller
 {
-	public function __construct(Request $request)
-    {
-        if (env('SHEETS_EXTERNAL')) {
-            $this->headers = [
+
+    public function init(Request $request){
+
+        if (env('SHEETS_EXTERNAL') || !Auth::user()) {
+
+            $this->clientBuilder = [
+                'http_errors' => false,
+                'verify' => false,
+                'base_uri' => env('SHEETS_API_URL'),
+            ];
+            $this->clientBuilder['headers'] = [
                 'Content-Type' => 'application/json',
                 'AccessToken' => 'key',
                 'Authorization' => env('SECURITY_BEARER'),
             ];
 
-            $this->clientBuilder = [
-                'headers' => $this->headers,
-                'http_errors' => false,
-                'verify' => false,
-                'base_uri' => env('SHEETS_API_URL'),
-            ];
-        }
-        else {
+        }else{
+
             $this->clientBuilder = [
                 'http_errors' => false,
                 'verify' => false,
                 'base_uri' => env('APP_URL'),
             ];
+            $this->clientBuilder['headers'] = collect($request->header())->transform(function ($item) {
+                return $item[0];
+            })->toArray();
         }
+
     }
-	
+    
     public function index($id,Request $request)
     {
-       
-        $method = 'get';
+        $this->init($request);
+
         $endpoint = "form/{$id}";
         $response = null;
-	   
-		if (!env('SHEETS_EXTERNAL')) {
-            $headers = collect($request->header())->transform(function ($item) {
-                return $item[0];
-            });
-
-            $this->clientBuilder['headers'] = $headers->toArray();
-        }
-      
-      
+        
         $client = new GuzzleClient($this->clientBuilder);
         $response = $client->request('GET', $endpoint);
 
@@ -60,17 +56,10 @@ class SheetsController extends Controller
 
     public function getrecord($entityname,$recordid,Request $request)
     {
-        $endpoint =  'entity/data/' . $entityname . '/' . $recordid;
-        $response = null;
-	        		
-		 if (!env('SHEETS_EXTERNAL')) {
-            $headers = collect($request->header())->transform(function ($item) {
-                return $item[0];
-            });
+        $this->init($request);
 
-            $this->clientBuilder['headers'] = $headers->toArray();
-        }
-		
+        $endpoint =  'entity/data/' . $entityname . '/' . $recordid;
+        
         $client = new GuzzleClient($this->clientBuilder);
         $response = $client->request('GET', $endpoint);
 
@@ -79,32 +68,21 @@ class SheetsController extends Controller
 
     public function saveFile(Request $request)
     {
-        try {
-            $id = $request->fileId;
+        $this->init($request);
 
+        try {
+
+            $id = $request->fileId;
             $file = $request->file('file');
             $name = $file->getClientOriginalName();
-
             $file->move(storage_path('app'), $name);
-
             $path =  storage_path('app')."/{$name}";
 
-            $method = 'POST';
-            $baseUrl = env('SHEETS_API_URL') . 'document';
+            $endpoint = 'document';
+            $this->clientBuilder['headers']["Content-Type"] = "multipart/form-data" ;
+            $client = new GuzzleClient($this->clientBuilder);
 
-            $headers = [
-                'AccessToken' => 'key',
-                'Authorization' => env('SECURITY_BEARER'),
-                "Content-Type" => "multipart/form-data"
-            ];
-
-            $client = new GuzzleClient([
-                'headers' => $headers,
-                'http_errors' => false,
-                'verify' => false
-            ]);
-
-            $r = $client->request($method, $baseUrl, [
+            $r = $client->request('POST', $endpoint, [
                 'multipart' => [
                     [
                         'contents' => file_get_contents($path),
@@ -113,11 +91,10 @@ class SheetsController extends Controller
                     ]
                 ]
             ]);
-            $res = new \stdClass();
-
+            $res          = new \stdClass();
             $res->success = $r->getStatusCode() === 200;
-				  
             $res->content = json_decode($r->getBody()->getContents());
+            $res->status  = $r->getStatusCode();
 
             return response()->json($res);
 
@@ -128,32 +105,27 @@ class SheetsController extends Controller
 
     public function sendForm(Request $request)
     {
-        try {
-            $entity_key = $request->entityKey;
-            $entity = new \stdClass();
-            $entity->$entity_key = \json_decode($request->$entity_key);
-            $method = 'post';
-            $baseUrl = env('SHEETS_API_URL') . 'entity';
-            $response = null;
-            $headers = [
-                'Content-Type' => 'application/json',
-                'AccessToken' => 'key',
-                'Authorization' => env('SECURITY_BEARER'),
-            ];
-            $client = new GuzzleClient([
-                'headers' => $headers,
-                'http_errors' => false,
-                'verify' => false,
-                \GuzzleHttp\RequestOptions::JSON => $entity,
-            ]);
-            $r = $client->request($method, $baseUrl);
-           $res = new \stdClass();
+        $this->init($request);
 
+        try {
+
+            $entity = new \stdClass();
+            $entity_key = $request->entityKey;
+            $entity->$entity_key = \json_decode($request->$entity_key);
+            
+            $endpoint = 'entity';
+            $this->clientBuilder[\GuzzleHttp\RequestOptions::JSON] = $entity;
+            $client   = new GuzzleClient($this->clientBuilder);
+
+            $r = $client->request('post', $endpoint);
+            
+            $res          = new \stdClass();
             $res->success = $r->getStatusCode() === 200;
             $res->content = json_decode($r->getBody()->getContents());
-            $res->status = $r->getStatusCode();
+            $res->status  = $r->getStatusCode();
 
-            return response()->json($res, $r->getStatusCode());
+            return response()->json($res, $res->status);
+
         } catch (\Throwable $th) {
             return response()->json(['success' => false, 'content' => json_encode($th->getMessage())]);
         }
@@ -161,58 +133,47 @@ class SheetsController extends Controller
 
     public function sendFormUpdate(Request $request)
     {
-        try {
-            $entity_key = $request->entityKey;
-            $entity = new \stdClass();
-            $entity->$entity_key = \json_decode($request->$entity_key);
-            $method = 'post';
-            $baseUrl = env('SHEETS_API_URL') . 'entity';
-            $response = null;
-            $headers = [
-                'Content-Type' => 'application/json',
-                'AccessToken' => 'key',
-                'Authorization' => env('SECURITY_BEARER'),
-            ];
-            // return response()->json(['success' => false, 'content' => [\GuzzleHttp\RequestOptions::JSON => $entity]]);
-            $client = new GuzzleClient([
-                'headers' => $headers,
-                'verify' => false,
-                'http_errors' => false,
-                \GuzzleHttp\RequestOptions::JSON => $entity,
-            ]);
-            $r = $client->request($method, $baseUrl);
-          $res = new \stdClass();
+        $this->init($request);
 
+        try {
+
+            $entity = new \stdClass();
+            $entity_key = $request->entityKey;
+            $entity->$entity_key = \json_decode($request->$entity_key);
+
+            $endpoint = 'entity';
+            $this->clientBuilder[\GuzzleHttp\RequestOptions::JSON] = $entity;
+            $client   = new GuzzleClient($this->clientBuilder);
+
+            $r = $client->request('post', $endpoint);
+
+            $res          = new \stdClass();
             $res->success = $r->getStatusCode() === 200;
             $res->content = json_decode($r->getBody()->getContents());
-            $res->status = $r->getStatusCode();
+            $res->status  = $r->getStatusCode();
 
-            return response()->json($res, $r->getStatusCode());
+            return response()->json($res, $res->status);
+
         } catch (\Throwable $th) {
             return response()->json(['success' => false, 'content' => $th->getMessage()]);
         }
     }
 
-    public function entity_info($id)
+    public function entity_info($id,Request $request)
     {
-		 try {
-        $endpoint = env('SHEETS_API_URL') . "entity/info/{$id}";
-        $headers = [
-            'Content-Type' => 'application/json',
-            'AccessToken' => 'key',
-            'Authorization' => env('SECURITY_BEARER'),
-        ];
-        $client = new GuzzleClient([
-            'headers' => $headers,
-            'http_errors' => false,
-            'verify' => false,
-        ]);
-            $res = $client->request('GET', $endpoint);
-            $response = new \stdClass();
+        $this->init($request);
 
+        try {
+
+            $endpoint = "entity/info/{$id}";
+            $client   = new GuzzleClient($this->clientBuilder);
+            
+            $res = $client->request('GET', $endpoint);
+
+            $response          = new \stdClass();
             $response->success = $res->getStatusCode() === 200;
             $response->content = json_decode($res->getBody()->getContents());
-            $response->status = $res->getStatusCode();
+            $response->status  = $res->getStatusCode();
 
             return response()->json($response, $response->status);
 
