a620c3b275d574002e863c01a257fa0d3277cead
Eduardo Alvarado <eduardoalvara2@gmail.com>
2021-09-16 18:57:22 -0300

    client update

--- resources/js/Sheets/core/new_components/inputs/text.vue
+++ resources/js/Sheets/core/new_components/inputs/text.vue
@@ -4,29 +4,31 @@
             type="text"
             :id="id"
             :value="inputValue"
+            :disabled="disabled"
             @input="onInput"
             class="form-control">
     </form-group>
 </template>
 
 <script>
-import mix from '../mixs/input.vue'
-import FormGroup from '../templates/form-group.vue'
+import mix from '../mixs/input.vue';
+import FormGroup from '../templates/form-group.vue';
+
 export default {
     mixins: [mix],
     components: {
-        'form-group': FormGroup
+        'form-group': FormGroup,
     },
     watch: {
         inputValue(val) {
-            let data = {
+            const data = {
                 col_name: this.input.col_name,
-                text: val
-            }
-            this.$store.commit(`${this.state}/SEARCH_MAP`, data)
-        }
-    }
-}
+                text: val,
+            };
+            this.$store.commit(`${this.state}/SEARCH_MAP`, data);
+        },
+    },
+};
 </script>
 
 <style>
