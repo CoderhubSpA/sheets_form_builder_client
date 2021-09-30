320465b06b87af61d70fac80a0c3e677eadb5e09
Eduardo Alvarado <eduardoalvara2@gmail.com>
2021-09-30 13:32:03 -0300

    update

--- resources/js/store/formBuilder.js
+++ resources/js/store/formBuilder.js
@@ -34,7 +34,7 @@ export default {
         form_id: '',
         record_id: null,
         errors_fields: {},
-        smarequiredfields:[],
+        smarequiredfields: [],
         errorssma: null,
         /**
          * Si “default:1”, el Form Builder no debe enviar al backend:
@@ -47,36 +47,37 @@ export default {
          * respuesta del server sin tratar
          */
         raw: {},
-
     }),
     getters: {
-        loading: state => state.loading,
-        entity_id: state => state.entityId,
-        entity_name: state => state.entityName,
-        content_info: state => state.contentInfo,
-        hasFiles: state => state.hasFiles,
-        files: state => state.files,
-        fields: state => state.fields,
-        pivots: state => state.pivots,
-        searchMap: state => state.searchMap,
-        history: state => state.history,
-        poll_sections: state => state.poll_sections,
-        poll_questions: state => state.poll_questions,
-        poll_active_section: state => state.poll_active_section,
-        clearfields: state => state.clearfields,
-        smarequiredfields: state => state.smarequiredfields,
-        errorssma: state => state.errorssma,
+        loading: (state) => state.loading,
+        entity_id: (state) => state.entityId,
+        entity_name: (state) => state.entityName,
+        content_info: (state) => state.contentInfo,
+        hasFiles: (state) => state.hasFiles,
+        files: (state) => state.files,
+        fields: (state) => state.fields,
+        pivots: (state) => state.pivots,
+        searchMap: (state) => state.searchMap,
+        history: (state) => state.history,
+        poll_sections: (state) => state.poll_sections,
+        poll_questions: (state) => state.poll_questions,
+        poll_active_section: (state) => state.poll_active_section,
+        clearfields: (state) => state.clearfields,
+        smarequiredfields: (state) => state.smarequiredfields,
+        errorssma: (state) => state.errorssma,
         last_section: (state) => {
-            const index = state.history.length -1
+            const index = state.history.length - 1;
 
-            const section = state.poll_sections.find(s => s.id === state.history[index].section_id)
+            const section = state.poll_sections.find(
+                (s) => s.id === state.history[index].section_id
+            );
 
-            return section
+            return section;
         },
-        form_id: state => state.form_id,
-        record_id: state => state.record_id,
-        errors_fields: state => state.errors_fields,
-        default_form: state => state.default_form,
+        form_id: (state) => state.form_id,
+        record_id: (state) => state.record_id,
+        errors_fields: (state) => state.errors_fields,
+        default_form: (state) => state.default_form,
         raw: (state) => state.raw,
         name: (state) => state.name,
     },
@@ -84,11 +85,11 @@ export default {
         LOADING(state, val) {
             state.loading = val;
         },
-        SMAREQUIREDFIELDS(state, val){
-            state.smarequiredfields.push(val)
+        SMAREQUIREDFIELDS(state, val) {
+            state.smarequiredfields.push(val);
         },
-        ERRORSSMA(state, val){
-            state.errorssma = val
+        ERRORSSMA(state, val) {
+            state.errorssma = val;
         },
         ENTITY_ID(state, val) {
             state.entityId = val;
@@ -110,12 +111,12 @@ export default {
             let newKey = Object.keys(val)[0];
             let found = false;
             state.fields.map((field, index) => {
-                if(Object.keys(field)[0] === newKey){
+                if (Object.keys(field)[0] === newKey) {
                     state.fields[index] = val;
                     found = true;
                 }
             });
-            if(!found){
+            if (!found) {
                 state.fields.push(val);
             }
         },
@@ -169,7 +170,8 @@ export default {
             const params = data.params;
             // const URL = req.record_id ? `/api/sheets/form/${req.entity}/${req.record_id}` :
             return new Promise((resolve, reject) => {
-                axios.get(`/api/sheets/form/${id}`)
+                axios
+                    .get(`/api/sheets/form/${id}`)
                     .then((response) => {
                         const data = response.data.content;
                         commit('RAW', data);
@@ -179,11 +181,11 @@ export default {
                             actions = data.actions.filter((action) => action.valid !== 0);
                         else actions = [DEFAULT_ACTION];
                         // console.log(params)
-                        if(params && params.length > 0){
+                        if (params && params.length > 0) {
                             const parametrosJson = JSON.parse(params);
                             data.fields.map((item) => {
                                 Object.keys(parametrosJson).forEach((paramkey) => {
-                                    if(paramkey === item.col_name){
+                                    if (paramkey === item.col_name) {
                                         let f = {};
                                         f[item.id] = parametrosJson[paramkey];
                                         commit('FIELDS', f);
@@ -202,37 +204,41 @@ export default {
 
                         let rows = data.rows.map((row) => {
                             let sections = data.sections.filter((sect) => {
-                                return sect.form_row_id === row.id
-                            })
+                                return sect.form_row_id === row.id;
+                            });
 
                             sections.sort((a, b) => {
                                 return a.order > b.order ? 1 : -1;
-                            })
+                            });
 
-                            sections.map(section => {
+                            sections.map((section) => {
                                 let fields = [];
                                 if (Array.isArray(data.fields)) {
                                     fields = data.fields.filter((f) => {
-                                        return f.form_section_id === section.id && (f.permission !== 0 || (data.default && f.permission === 0))
-                                    })
+                                        return (
+                                            f.form_section_id === section.id &&
+                                            (f.permission !== 0 ||
+                                                (data.default && f.permission === 0))
+                                        );
+                                    });
                                 } else {
                                     fields = [...data.fields];
                                 }
                                 fields.sort((a, b) => {
-                                    return a.order > b.order ? 1 : -1
-                                })
-                                section.fields = fields
-                            })
-                            row.sections = sections
-                            return row
+                                    return a.order > b.order ? 1 : -1;
+                                });
+                                section.fields = fields;
+                            });
+                            row.sections = sections;
+                            return row;
                         });
                         rows.sort((a, b) => {
-                            return a.order > b.order ? 1 : -1
+                            return a.order > b.order ? 1 : -1;
                         });
                         let form = {
                             rows: rows,
                             actions: actions.sort((a, b) => {
-                                return a.save_form > b.save_form ? 1 : -1
+                                return a.save_form > b.save_form ? 1 : -1;
                             }),
                             success: response.data.success,
                             message: response.data.message,
@@ -254,48 +260,52 @@ export default {
             });
         },
         update({ commit }, data) {
-            commit('LOADING', true)
+            commit('LOADING', true);
             return new Promise((resolve, reject) => {
-                axios.post(`/api/sheets/save/form/update`, data, {})
-                .then(response => {
-                    resolve(response.data)
-                }).catch(error => {
-                    reject(error);
-                }).finally(() => {
-                    commit('LOADING', false)
-                });
+                axios
+                    .post(`/api/sheets/save/form/update`, data, {})
+                    .then((response) => {
+                        resolve(response.data);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    })
+                    .finally(() => {
+                        commit('LOADING', false);
+                    });
             });
         },
-        get_record({ commit}, data) {
-            commit('LOADING', true)
+        get_record({ commit }, data) {
+            commit('LOADING', true);
             commit('RECORD_ID', data.id);
             return new Promise((resolve, reject) => {
-                axios.get(`/api/sheets/getrecord/${data.entity_name}/${data.id}`)
-                .then(response => {
-                    const fields = response.data.content.data[0]
-                    Object.keys(fields).forEach(key => {
-                        let f = {}
-                        f[key] = fields[key]
+                axios
+                    .get(`/api/sheets/getrecord/${data.entity_name}/${data.id}`)
+                    .then((response) => {
+                        const fields = response.data.content.data[0];
+                        Object.keys(fields).forEach((key) => {
+                            let f = {};
+                            f[key] = fields[key];
 
-                        commit('FIELDS', f)
+                            commit('FIELDS', f);
+                        });
+                        commit('PIVOTS', response.data.content.pivots);
+                        resolve(response.data);
                     })
-                    commit('PIVOTS', response.data.content.pivots);
-                    resolve(response.data)
-                })
-                .catch(error => {
-                    console.log(error)
-                    reject(error)
-                })
-                .finally(() => {
-                    commit('LOADING', false)
-                })
-            })
-
+                    .catch((error) => {
+                        console.log(error);
+                        reject(error);
+                    })
+                    .finally(() => {
+                        commit('LOADING', false);
+                    });
+            });
         },
         async info({ commit }, id) {
             if (id) {
                 commit('LOADING', true);
-                await axios.get(`/api/sheets/entity/info/${id}`)
+                await axios
+                    .get(`/api/sheets/entity/info/${id}`)
                     .then((response) => {
                         commit('CONTENT_INFO', response.data.content);
                     })
@@ -308,83 +318,92 @@ export default {
             }
         },
         save({ commit }, data) {
-            commit('LOADING', true)
+            commit('LOADING', true);
             return new Promise((resolve, reject) => {
-                axios.post(`/api/sheets/save/form`, data)
-                .then(response => {
-                    resolve(response.data)
-                })
-                .catch(error => {
-                    if (error.response.data.content) {
-                        const content = error.response.data.content
-                        if(typeof content === 'object'){
-                            Object.keys(content.errors).map(key => {
-                                let value = {
-                                    key: key,
-                                    value: content.errors[key]
+                axios
+                    .post(`/api/sheets/save/form`, data)
+                    .then((response) => {
+                        resolve(response.data);
+                    })
+                    .catch((error) => {
+                        if (error.response.data.content) {
+                            const content = error.response.data.content;
+                            if (typeof content === 'object') {
+                                if (typeof content.errors === 'object') {
+                                    Object.keys(content.errors).map((key) => {
+                                        let value = {
+                                            key: key,
+                                            value: content.errors[key],
+                                        };
+                                        commit('ERRORS_FIELD', value);
+                                    });
                                 }
-                                commit('ERRORS_FIELD', value)
-                            })
+                            }
                         }
-                    }
-                    reject(error.response)
-                })
-                .finally(() => {
-                    commit('LOADING', false)
-                })
-            })
+                        reject(error.response);
+                    })
+                    .finally(() => {
+                        commit('LOADING', false);
+                    });
+            });
         },
         async upload_files({ commit }, data) {
-            commit('LOADING', true)
+            commit('LOADING', true);
             return new Promise((resolve, reject) => {
                 const headers = {
                     headers: {
-                        "Content-Type": "multipart/form-data"
-                    }
-                }
-                axios.post(`/api/sheets/save/file`, data, headers)
-                .then(response => {
-                    const id = response.data.content.content.inserted_id
-                    resolve(id)
-                })
-                .catch(error => {
-                    reject(error)
-                })
-                .finally(() => {
-                    commit('LOADING', false)
-                })
+                        'Content-Type': 'multipart/form-data',
+                    },
+                };
+                axios
+                    .post(`/api/sheets/save/file`, data, headers)
+                    .then((response) => {
+                        const id = response.data.content.content.inserted_id;
+                        resolve(id);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    })
+                    .finally(() => {
+                        commit('LOADING', false);
+                    });
             });
         },
         get_poll({ commit, dispatch }, id) {
-            commit('LOADING', true)
+            commit('LOADING', true);
             return new Promise((resolve, reject) => {
-                axios.get(`/api/sheets/form/${id}`)
-                .then(response => {
-                    const data = response.data.content
+                axios
+                    .get(`/api/sheets/form/${id}`)
+                    .then((response) => {
+                        const data = response.data.content;
 
-                    commit('POLL_SECTIONS', data.sections)
+                        commit('POLL_SECTIONS', data.sections);
 
-                    const active_section = data.sections.find(sec => sec.visible_on_load === 1)
-                    commit('POLL_ACTIVE_SECTION', active_section)
+                        const active_section = data.sections.find(
+                            (sec) => sec.visible_on_load === 1
+                        );
+                        commit('POLL_ACTIVE_SECTION', active_section);
 
-                    commit('POLL_QUESTIONS', data.fields)
+                        commit('POLL_QUESTIONS', data.fields);
 
-                    const actions = data.actions.sort((a, b) => { return a.save_form > b.save_form ? 1 : -1})
-                    // commit('POLL_ACTIONS', actions)
-                    return data.entity_type_id
-                })
-                .then(entity_type_id => {
-                    if (!!entity_type_id) {
-                        dispatch('info', entity_type_id)
-                    }
-                })
-                .catch(error => {
-                    reject(error)
-                })
-                .finally(() => {
-                    commit('LOADING', false)
-                })
-            })
+                        const actions = data.actions.sort((a, b) => {
+                            return a.save_form > b.save_form ? 1 : -1;
+                        });
+                        // commit('POLL_ACTIONS', actions)
+                        return data.entity_type_id;
+                    })
+                    .then((entity_type_id) => {
+                        if (!!entity_type_id) {
+                            dispatch('info', entity_type_id);
+                        }
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    })
+                    .finally(() => {
+                        commit('LOADING', false);
+                    });
+            });
         },
     },
 };
