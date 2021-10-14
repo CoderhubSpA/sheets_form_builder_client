818ab4492f3970cc974f4e5d056805f34ceeb6e1
Eduardo Alvarado <eduardoalvara2@gmail.com>
2021-10-14 14:20:30 -0300

    update

--- resources/js/Sheets/core/new_components/inputs/selector-1xn-onlynew.vue
+++ resources/js/Sheets/core/new_components/inputs/selector-1xn-onlynew.vue
@@ -15,7 +15,6 @@
         :no-drop="true"
         :multiple="multiple"
         v-model="selected"
-        @option:deselected="deselected"
       >
       </v-select>
       <nested-form
@@ -48,12 +47,6 @@ export default {
       return true;
     },
   },
-  methods: {
-    deselected(val) {
-      const item = this.selected.find((option) => option.id === val.id);
-      const index = this.selected.indexOf(item);
-      this.selected.splice(index, 1);
-    },
-  },
+  methods: {},
 };
 </script>
