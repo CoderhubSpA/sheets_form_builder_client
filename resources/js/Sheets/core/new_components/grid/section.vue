be3a238189fba50037be7ea1fadb0f21cd3b1001
Eduardo Alvarado <eduardoalvara2@gmail.com>
2021-11-24 15:14:42 -0300

    Form Builder update

--- resources/js/Sheets/core/new_components/grid/section.vue
+++ resources/js/Sheets/core/new_components/grid/section.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`${sm} ${md} ${xl}`" v-if="show_section" :id="id">
+  <div :class="`${sm} ${md} ${xl}`" v-if="show_section">
     <h5 class="sheets-section-title">
       {{ name }}
     </h5>
@@ -42,69 +42,67 @@ export default {
       type: String,
       require: true,
     },
-    computed: {
-        id() {
-            return this.section.id;
-        },
-        name() {
-            return this.section.name;
-        },
-        sm() {
-            return `col-sm-${this.section.col_sm}`;
-        },
-        md() {
-            return `col-md-${this.section.col_md}`;
-        },
-        xl() {
-            return `col-xl-${this.section.col_xl}`;
-        },
-        fields() {
-            return this.section.fields || [];
-        },
-        img() {
-            let path = '';
-            if (this.section.image) {
-                const url = `${window.location.protocol}//${window.location.host}/`;
-                // eslint-disable-next-line camelcase
-                const base_url = this.$store.getters[`${this.state}/base_url`] || url;
-                path = new URL(this.section.image, base_url).toString();
-            }
-            return path;
-        },
-        show_by_field_id() {
-            return this.section.show_by_field_id;
-        },
-        show_by_field_value() {
-            return this.section.show_by_field_value;
-        },
-        show_section() {
-            const fields = this.$store.getters[`${this.state}/field_section_show_hide`];
-            // eslint-disable-next-line camelcase
-            let show_section = false;
-            if (this.show_by_field_id) {
-                // eslint-disable-next-line eqeqeq
-                if ((fields[this.show_by_field_id] || false) == this.show_by_field_value) {
-                    // eslint-disable-next-line camelcase
-                    show_section = true;
-                }
-                try {
-                    // Permite validar si entre un selector multiple existe el
-                    // valor que condiciona que el elemento se vea o no
-                    if (
-                        Array.isArray(fields[this.show_by_field_id]) &&
-                        fields[this.show_by_field_id]
-                            .map((d) => d.id)
-                            .includes(this.show_by_field_value)
-                    ) {
-                        // eslint-disable-next-line camelcase
-                        show_section = true;
-                    }
-                } catch (e) {
-                    // eslint-disable-next-line no-console
-                    console.warn(e);
-                }
-                // eslint-disable-next-line camelcase
-            } else show_section = true;
+  },
+  data: () => ({
+    sectionModel: [],
+  }),
+  watch: {
+    sectionModel() {
+      this.$emit('input', this.sectionModel);
+    },
+  },
+  computed: {
+    id() {
+      return this.section.id;
+    },
+    name() {
+      return this.section.name;
+    },
+    sm() {
+      return `col-sm-${this.section.col_sm}`;
+    },
+    md() {
+      return `col-md-${this.section.col_md}`;
+    },
+    xl() {
+      return `col-xl-${this.section.col_xl}`;
+    },
+    fields() {
+      return this.section.fields || [];
+    },
+    img() {
+      let path = '';
+      if (this.section.image) {
+        const url = `${window.location.protocol}//${window.location.host}/`;
+        // eslint-disable-next-line camelcase
+        const base_url = this.$store.getters[`${this.state}/base_url`] || url;
+        path = new URL(this.section.image, base_url).toString();
+      }
+      return path;
+    },
+    show_by_field_id() {
+      return this.section.show_by_field_id;
+    },
+    show_by_field_value() {
+      return this.section.show_by_field_value;
+    },
+    show_section() {
+      const fields = this.$store.getters[`${this.state}/field_section_show_hide`];
+      // eslint-disable-next-line camelcase
+      let show_section = false;
+      if (this.show_by_field_id) {
+        // eslint-disable-next-line eqeqeq
+        if ((fields[this.show_by_field_id] || false) == this.show_by_field_value) {
+          // eslint-disable-next-line camelcase
+          show_section = true;
+        }
+        try {
+          // Permite validar si entre un selector multiple existe el
+          // valor que condiciona que el elemento se vea o no
+          if (
+            Array.isArray(fields[this.show_by_field_id]) &&
+            fields[this.show_by_field_id].map((d) => d.id).includes(this.show_by_field_value)
+          ) {
             // eslint-disable-next-line camelcase
             show_section = true;
           }
