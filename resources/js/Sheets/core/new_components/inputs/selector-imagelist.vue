eb078c30a0ec276a69d13a752301a7dd18e03cc4
Eduardo Alvarado <eduardoalvara2@gmail.com>
2022-08-25 16:53:00 -0400

    Update Form Builder automatically

--- resources/js/Sheets/core/new_components/inputs/selector-imagelist.vue
+++ resources/js/Sheets/core/new_components/inputs/selector-imagelist.vue
@@ -33,8 +33,8 @@
                                 </div>
                                 <div class="selected-indicator">
                                     <i
-                                        :class="['bi bi-dash-circle-fill',
-                                            { indicator: !option.selected }
+                                        :class="['bi bi-dash-circle-fill', 'no-indicator',
+                                            { 'indicator': option.selected }
                                         ]"
                                     ></i>
                                 </div>
