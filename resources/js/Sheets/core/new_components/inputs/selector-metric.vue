f40efae9e762d66a41dcb5a1e0a403b0aac2ce6d
Eduardo Alvarado <eduardoalvara2@gmail.com>
2022-09-21 19:11:05 -0300

    Update Form Builder automatically

---
+++ resources/js/Sheets/core/new_components/inputs/selector-metric.vue
@@ -0,0 +1,20 @@
+<template>
+    <selector-remote
+        :input="input"
+        :value="value"
+        :state="state"
+        :base_url="base_url"
+    ></selector-remote>
+</template>
+
+<script>
+import mix from '../mixs/input.vue';
+import SelectorRemote from './selector-remote.vue';
+
+export default {
+    mixins: [mix],
+    components: {
+        'selector-remote': SelectorRemote,
+    },
+};
+</script>
