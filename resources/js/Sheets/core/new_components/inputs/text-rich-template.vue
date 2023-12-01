f2626fc018fac42ed1bb6058321fee285a84d889
Eduardo Alvarado <eduardoalvara2@gmail.com>
2023-12-01 17:33:03 -0300

    Update Form Builder automatically

--- resources/js/Sheets/core/new_components/inputs/text-rich-template.vue
+++ resources/js/Sheets/core/new_components/inputs/text-rich-template.vue
@@ -9,7 +9,7 @@
         v-if="show_field"
     >
         <Editor
-            api-key="no-api-key"
+            api-key="vdjygk7n8vw4laxwpwsmh1sb36zu318ltnp3u9f9wcrqx83g"
             :init="tinymceConfig"
             :id="id"
             v-if="tinymceConfig"
@@ -52,12 +52,90 @@ export default {
                 })
                 .map((column) => {
                     return {
-                        name: `${response.data.content.content.entity_type.name} : ${column.name}`,
+                        name: column.name,
+                        entity_name: response.data.content.content.entity_type.name,
                         id: column.id,
                         entity_type_permission_fk: column.entity_type_permission_fk,
                         subdata: null,
+                        format: column.format,
                     };
                 });
+        },
+        async generateDropDownItems(filteredData) {
+            const hasEntityTypePermissionFk = filteredData.some(
+                (column) => column.entity_type_permission_fk
+            );
+
+            if (hasEntityTypePermissionFk) {
+                const groupedColumns = {};
+                const usedEntities = {};
+                // Se agrupan los elementos con la misma entity_type_permission_fk, de esa forma no aparecen duplicados en el dropdown
+                filteredData.forEach((column) => {
+                    if (column.entity_type_permission_fk) {
+                        if (!groupedColumns[column.entity_type_permission_fk]) {
+                            groupedColumns[column.entity_type_permission_fk] = {
+                                name: '',
+                                entity_name: column.entity_name,
+                                column: column,
+                                subdata: null,
+                            };
+                        }
+                        groupedColumns[column.entity_type_permission_fk].name += column.name + ', ';
+                    }
+                });
+                // Se realiza el fetch de la data de las columnas agrupadas
+                await Promise.all(
+                    Object.values(groupedColumns).map((group) => this.fetchSubdata(group.column))
+                );
+
+                //Tomando la data original, la transformamos a los elementos finales para el dropdown
+                return filteredData.reduce((acc, item) => {
+                    // Si el elemento tiene un entity_type_permission_fk y no ha sido usado, se agrega al dropdown
+                    if (
+                        item.entity_type_permission_fk &&
+                        !usedEntities[item.entity_type_permission_fk]
+                    ) {
+                        acc[item.id] = {
+                            name: `${
+                                groupedColumns[item.entity_type_permission_fk].entity_name
+                            } : ${groupedColumns[item.entity_type_permission_fk].name.slice(
+                                0,
+                                -2
+                            )}`,
+                            format: item.format,
+                            values: groupedColumns[
+                                item.entity_type_permission_fk
+                            ].column.subdata.reduce((subAcc, subitem) => {
+                                subAcc[subitem.id] = {
+                                    name: subitem.name,
+                                    id: subitem.id,
+                                };
+                                return subAcc;
+                            }, {}),
+                        };
+                        usedEntities[item.entity_type_permission_fk] = true;
+                    }
+                    // Si el elemento tiene un entity_type_permission_fk y ya ha sido usado, no se agrega al dropdown
+                    else if (
+                        item.entity_type_permission_fk &&
+                        usedEntities[item.entity_type_permission_fk]
+                    ) {
+                        return acc;
+                    }
+                    // Si el elemento no tiene un entity_type_permission_fk, se agrega al dropdown
+                    else {
+                        acc[item.id] = {
+                            name: `${item.entity_name} : ${item.name}`,
+                        };
+                    }
+                    return acc;
+                }, {});
+            }
+
+            return filteredData.reduce((acc, item) => {
+                acc[item.id] = { name: item.name };
+                return acc;
+            }, {});
         },
         // Si una columna tiene entity_type_permission_fk se realiza nuevamente la request de sus columnas
         async fetchSubdata(column) {
@@ -69,29 +147,6 @@ export default {
                 column.subdata = columnfilteredData;
             }
         },
-        //Se generan los items para el dropdown. Primero chequea si existe algun item con entidad relacionada, en caso de que si, se realiza la request de sus columnas
-        async generateDropDownItems(filteredData) {
-            const hasEntityTypePermissionFk = filteredData.some(
-                (column) => column.entity_type_permission_fk
-            );
-
-            if (hasEntityTypePermissionFk) {
-                await Promise.all(filteredData.map((column) => this.fetchSubdata(column)));
-            }
-
-            return filteredData.reduce((acc, item) => {
-                acc[item.id] = item.subdata
-                    ? {
-                          name: item.name,
-                          values: item.subdata.reduce((subAcc, subitem) => {
-                              subAcc[subitem.id] = { name: subitem.name, id: subitem.id };
-                              return subAcc;
-                          }, {}),
-                      }
-                    : { name: item.name };
-                return acc;
-            }, {});
-        },
         async onEntitySelected(entityTypeId) {
             //Extract content from tinymce editor
             if (entityTypeId == null) {
@@ -102,14 +157,14 @@ export default {
             }
             try {
                 const filteredData = await this.fetchValidColumns(entityTypeId);
+                // Extract all items in filteredData that have a format of "SELECTOR[MULTIPLE]" in another variable and the rest pass to generateDropDownItems
                 const dropDownItemsFromData = await this.generateDropDownItems(filteredData);
                 this.tinymceConfig = {
-                    branding: false,
-                    plugins: 'pagebreak table',
-                    promotion: false,
+                    plugins: 'pagebreak table link image lists',
+                    table_sizing_mode: 'relative',
                     height: 600,
                     toolbar:
-                        'undo redo print spellcheckdialog formatpainter | blocks fontfamily fontsize | bold italic underline forecolor backcolor | link image | alignleft aligncenter alignright alignjustify lineheight | checklist bullist numlist indent outdent | removeformat | mybutton pagebreak',
+                        'undo redo | mybutton | blocks | bold italic underline forecolor | link image | alignleft aligncenter alignright alignjustify lineheight | bullist numlist indent outdent | removeformat |  pagebreak',
                     setup: function (editor) {
                         // Add a custom button to the toolbar
                         editor.ui.registry.addMenuButton('mybutton', {
@@ -124,7 +179,10 @@ export default {
                                             typeof values.values === 'object'
                                                 ? 'nestedmenuitem'
                                                 : 'menuitem';
-                                        if (type == 'nestedmenuitem') {
+                                        if (
+                                            type == 'nestedmenuitem' &&
+                                            values.format != 'SELECTOR[MULTIPLE]'
+                                        ) {
                                             return {
                                                 type: 'nestedmenuitem',
                                                 text: values.name,
@@ -154,18 +212,124 @@ export default {
                                                     );
                                                 },
                                             };
+                                        } else if (
+                                            type == 'nestedmenuitem' &&
+                                            values.format == 'SELECTOR[MULTIPLE]'
+                                        ) {
+                                            return {
+                                                type: 'menuitem',
+                                                text: values.name + '*',
+                                                onAction: () => {
+                                                    editor.execCommand('openCustomDialog', {
+                                                        id: id,
+                                                        values: values,
+                                                    });
+                                                },
+                                            };
                                         }
                                     }
                                 );
                                 callback(items);
                             },
                         });
+                        // Funcion custom para agregar el dialogo correspondiente a un selector multiple
+                        editor.addCommand('openCustomDialog', function (multiselectorItem) {
+                            // console.log(multiselectorItem);
+                            // console.log(
+                            //     Object.entries(
+                            //         multiselectorItem.values.values
+                            //     ).map(([id, values]) => {
+                            //         return {
+                            //             type: "checkbox",
+                            //             name: id,
+                            //             label: values.name,
+                            //             enabled: false,
+                            //         };
+                            //     })
+                            // );
+                            editor.windowManager.open({
+                                title: 'Insertar selector multiple',
+                                body: {
+                                    type: 'panel',
+                                    items: [
+                                        {
+                                            type: 'label',
+                                            label: 'Seleccionar las columnas que aparecerán en el documento como tabla',
+                                            items: Object.entries(
+                                                multiselectorItem.values.values
+                                            ).map(([id, values]) => {
+                                                return {
+                                                    type: 'grid',
+                                                    columns: 2,
+                                                    items: [
+                                                        {
+                                                            type: 'checkbox',
+                                                            name: id,
+                                                            label: values.name,
+                                                            enabled: false,
+                                                        },
+                                                        {
+                                                            type: 'input',
+                                                            name: id + 'orden',
+                                                            placeholder: 'Orden',
+                                                            inputMode: 'numeric',
+                                                        },
+                                                    ],
+                                                };
+                                            }),
+                                        },
+                                    ],
+                                },
+                                buttons: [
+                                    {
+                                        type: 'cancel',
+                                        text: 'Cancelar',
+                                    },
+                                    {
+                                        type: 'submit',
+                                        text: 'Insertar',
+                                        buttonType: 'primary',
+                                    },
+                                ],
+                                onSubmit: (api) => {
+                                    const data = api.getData();
+                                    const selectedColumns = Object.entries(
+                                        multiselectorItem.values.values
+                                    )
+                                        .map(([id, column]) => {
+                                            if (data[id] === false) {
+                                                return null;
+                                            }
+                                            return {
+                                                id,
+                                                name: column.name,
+                                                orden: data[id + 'orden'],
+                                            };
+                                        })
+                                        .filter((column) => column !== null)
+                                        .sort((a, b) => a.orden - b.orden);
+                                    const textToInsert = `&nbsp;<strong data-table-id="${
+                                        multiselectorItem.id
+                                    }" data-columns-ids="${selectedColumns
+                                        .map((column) => column.id)
+                                        .join(
+                                            ','
+                                        )}"><span contenteditable="false">{{<br/>Tabla de ${
+                                        multiselectorItem.values.name
+                                    } <br/> con columnas: <br/> ${selectedColumns
+                                        .map((column) => column.name)
+                                        .join('<br/>')}<br/>}}</span></strong>&nbsp;`;
+                                    tinymce.activeEditor.execCommand(
+                                        'mceInsertContent',
+                                        false,
+                                        textToInsert
+                                    );
+                                    api.close();
+                                },
+                            });
+                        });
                     },
                     pagebreak_separator: '<div class="pagebreak"></div>',
-                    init_instance_callback: function (_) {
-                        var freeTiny = document.querySelector('.tox .tox-notification--in');
-                        freeTiny.style.display = 'none';
-                    },
 
                     // Estilo para semejanza a word
                     content_style: `
